734748cf3ca138cc18e72c669c6de020cd3d23e0
RayWang0416 <2328324146@qq.com>
2024-11-04 18:06:11 -0800

    d3 dialog test

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 // todo
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.createElement("button");
+  button.textContent = "Click Me";
+
+  button.addEventListener("click", () => {
+    alert("You clicked the button!");
+  });
+
+  document.body.appendChild(button);
+});
